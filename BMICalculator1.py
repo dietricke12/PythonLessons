8e0f373a0d04c8c8f2e81fe123e1de89c8c71d91
Erika Dietrick (erdietri) <erdietri@cisco.com>
2021-02-19 15:39:01 -0500

    Simplified program by removing class Main for teaching purposes.

--- BMICalculator1.py
+++ BMICalculator1.py
@@ -3,25 +3,23 @@
 
 # First, we will welcome the user and ask for their height and weight. 
 
-class Main():
-    def __init__(self): 
-        print("Welcome to your personal BMI calculator! Please provide your height in inches.")
-        height = input()
-        print("Thank you. Now, please provide your weight in pounds.")
-        weight = input()
-        
-        # Next, we will convert height to kg and weight to m2.
-        
-        bmiHeight = float(height) / 0.0254
-        bmiWeight = float(weight) / 0.453592
-        
-        # Here is where we perform the BMI calculation.
-        
-        calcBMI = bmiHeight / bmiWeight
-        BMI = str(calcBMI)
-        
-        # Finally, we let the user know their BMI output.
-        
-        print (f'Your BMI is {BMI}.')
 
-Main()
+print("Welcome to your personal BMI calculator! Please provide your height in inches.")
+height = input()
+print("Thank you. Now, please provide your weight in pounds.")
+weight = input()
+        
+# Next, we will convert height to kg and weight to m2.
+        
+bmiHeight = float(height) / 0.0254
+bmiWeight = float(weight) / 0.453592
+        
+# Here is where we perform the BMI calculation.
+        
+calcBMI = bmiHeight / bmiWeight
+BMI = str(calcBMI)
+        
+# Finally, we let the user know their BMI output.
+        
+print (f'Your BMI is {BMI}.')
+        
